0014e6449e95cfb2b80c5a67d5f4f6e1eb8868ce
Ana-Maria-O <anamaria.olteniceanu@gmail.com>
2023-11-21 14:57:26 +0100

    Status display now also displays at the beginning.

--- StatusDisplay.java
+++ StatusDisplay.java
@@ -8,13 +8,12 @@ public class StatusDisplay {
                 double previousLength = RobotArm.length;
                 boolean previousGrip = RobotArm.gripped;
 
+                display();
+
                 while (true) {
                     // Check for changes in variables
                     if (RobotArm.rotation != previousRotation || RobotArm.height!= previousHeight || RobotArm.length!= previousLength || RobotArm.gripped!= previousGrip) {
-                        System.out.println("Current rotation (rad): " + RobotArm.rotation);
-                        System.out.println("Current height (cm): " + RobotArm.height);
-                        System.out.println("Current length (cm): " + RobotArm.length);
-                        System.out.println("Gripped: " + RobotArm.gripped);
+                    display();
                     }
                     
                 }
@@ -22,5 +21,12 @@ public class StatusDisplay {
         });
 
         displayThread.start();
+    }
+
+    private static void display() {
+        System.out.println("Current rotation (rad): " + RobotArm.rotation);
+        System.out.println("Current height (cm): " + RobotArm.height);
+        System.out.println("Current length (cm): " + RobotArm.length);
+        System.out.println("Gripped: " + RobotArm.gripped);
     }
 }
